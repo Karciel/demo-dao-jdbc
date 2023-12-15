7009d2e24592610094d7f64e02a9c0d388187984
Gabriel V. Silva <karciel@outlook.com>
2023-12-15 16:51:42 -0300

    findById implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -12,12 +12,28 @@ public class Department implements Serializable{
 	private String name;
 	
 	public Department() {
-	
 	}
 
 	public Department(Integer id, String name) {
 		super();
 		this.id = id;
+		this.name = name;
+	}
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 
